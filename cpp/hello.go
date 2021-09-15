20789b72c27fee9e0238229b4cbaf19c0ac818dc
superryanguo <superryanguo@outlook.com>
2021-09-15 22:42:09 +0800

    Make the CPP example

---
+++ cpp/hello.go
@@ -0,0 +1,13 @@
+package main
+
+// #include "hello.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+func main() {
+	cs := C.CString("Hello, World with cpp+go\n")
+	defer C.free(unsafe.Pointer(cs))
+	C.SayHello(cs)
+}
