4dabd93964a2a5989889bba88e5d2b3ee81a7172
ryanguo <superryanguo@outlook.com>
2022-02-12 18:59:41 +0800

    gocv file

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+.PHONY: clean
+clean: 
+	rm -f ./oledc/oled.o
+
 lib :
 	gcc -c ./oledc/oled.c -o ./oledc/oled.o -lwiringPi
 	ar cru ./oledc/liboledc.a ./oledc/oled.o
@@ -8,3 +12,5 @@ run: lib
 fax: lib
 	go run ./face/face.go 0 data/haarcascade_frontalface_default.xml
 
+gcv: cv
+	go run ./cv/cv.go 0
---
+++ cv/cv.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	deviceID := os.Args[1]
+
+	// open webcam
+	webcam, err := gocv.OpenVideoCapture(deviceID)
+	if err != nil {
+		fmt.Printf("error opening video capture device: %v\n", deviceID)
+		return
+	}
+	defer webcam.Close()
+
+	// open display window
+	window := gocv.NewWindow("CV")
+	defer window.Close()
+
+	// prepare image matrix
+	img := gocv.NewMat()
+	defer img.Close()
+
+	// color for the rect when faces detected
+	blue := color.RGBA{0, 0, 255, 0}
+	// show the image in the window, and wait 1 millisecond
+	fmt.Printf("Start reading device: %v\n", deviceID)
+
+	r := image.Rect(30, 30, 230, 230)
+
+	for {
+		if ok := webcam.Read(&img); !ok {
+			fmt.Printf("Device closed: %v\n", deviceID)
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+		gocv.Rectangle(&img, r, blue, 3)
+
+		size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+		pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+		gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
+
+		window.IMShow(img)
+		if window.WaitKey(1) >= 0 {
+			break
+		}
+
+	}
+}
